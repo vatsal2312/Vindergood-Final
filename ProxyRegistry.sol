fafede246f91eb52875e913d12dedddfaaf9a81a
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2022-02-24 18:08:36 +0530

    Update ProxyRegistry.sol

--- ProxyRegistry.sol
+++ ProxyRegistry.sol
@@ -8,7 +8,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.5;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Ownable.sol";
 
 import "./OwnableDelegateProxy.sol";
 import "./ProxyRegistryInterface.sol";
